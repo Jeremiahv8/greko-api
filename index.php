d9d870e8659ccf11966c0cf62a8bd1e190e3102a
Jeremiahv8 <jeremiah.cruickshank@hotmail.com>
2020-06-08 14:04:30 -0400

    comit on 'master'

--- index.php
+++ index.php
@@ -1,8 +1,10 @@
 <?php
-  echo "please";
   $response = [ 'message' => 'No command was selected!' ];
   $data = [ 'response' => $response ];
 
   header('Content-type: application/json');
   echo json_encode( $data );
+
+  echo "<script>console.log('" . $data . "');</script>";
+
 ?>
